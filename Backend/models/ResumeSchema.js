59436299afa81ea7c679a5f6ecdd1ec8392f5b20
aryaman22-hubb <aryamanpanwar187@gmail.com>
2025-03-16 18:05:00 +0530

    Review Implemented

--- Backend/models/ResumeSchema.js
+++ Backend/models/ResumeSchema.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const ResumeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     personalInfo: {

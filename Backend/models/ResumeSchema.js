d40303eb8ca6cf172155679179750a9a0878ceda
aryaman22-hubb <aryamanpanwar187@gmail.com>
2025-03-16 17:48:14 +0530

    Database Schema Created

---
+++ Backend/models/ResumeSchema.js
@@ -0,0 +1,42 @@
+const ResumeSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    personalInfo: {
+      fullName: { type: String, required: true },
+      email: { type: String, required: true },
+      phone: { type: String },
+      linkedin: { type: String },
+      github: { type: String }
+    },
+    education: [
+      {
+        institution: String,
+        degree: String,
+        startYear: String,
+        endYear: String,
+        required:true
+      }
+    ],
+    experience: [
+      {
+        company: String,
+        position: String,
+        startDate: String,
+        endDate: String,
+        description: String
+      }
+    ],
+    skills: [String],
+    projects: [
+      {
+        title: String,
+        description: String,
+        techStack: [String],
+        link: String
+      }
+    ],
+    generatedByAI: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+  });
+  
+  module.exports = mongoose.model('Resume', ResumeSchema);
+  
---
+++ Backend/models/Userschema.js
@@ -0,0 +1,13 @@
+// Consist of name , email , password , role(only user for new log in) , date(log in)
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, 
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);
